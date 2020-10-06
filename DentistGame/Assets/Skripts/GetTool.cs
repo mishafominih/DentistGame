9f400bb5d2667d02c1e30407a532617a9eddab97
mishafominih <mishagjxn@mail.ru>
2020-10-06 22:55:19 +0500

    Добавил чистку зубов

---
+++ DentistGame/Assets/Skripts/GetTool.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetTool : MonoBehaviour
+{
+    public GameObject Brush;
+
+    private GameObject canvas;
+    private GameObject realBrush;
+    private bool click;
+    private BoxCollider2D btn;
+    void Start()
+    {
+        btn = GetComponentInChildren<BoxCollider2D>();
+        canvas = GameObject.FindGameObjectWithTag("Canvas");
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0 && btn.bounds.Contains(Input.GetTouch(0).position))
+        {
+            click = true;
+            realBrush = Instantiate(Brush, canvas.transform);
+            btn.gameObject.GetComponent<Image>().enabled = false;
+            btn.enabled = false;
+        }
+        if (click && Input.touchCount > 0)
+        {
+            realBrush.transform.position = Input.GetTouch(0).position;
+            realBrush.transform.position += new Vector3(0, +200);
+        }
+        else
+        {
+            Destroy(realBrush);
+            click = false;
+            btn.gameObject.GetComponent<Image>().enabled = true;
+            btn.enabled = true;
+        }
+    }
+}
---
+++ DentistGame/Assets/Skripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer// : MonoBehaviour
+{
+    float timer = 0;
+    float time;
+    public Timer(float time)
+    {
+        this.time = time;
+    }
+
+    public float GetTime()
+    {
+        return timer > time ? time : timer;
+    }
+
+    public bool Check()
+    {
+        if(timer > time)
+        {
+            return true;
+        }
+        timer += Time.deltaTime;
+        return false;
+    }
+
+    public void Null()
+    {
+        timer = 0;
+    }
+}
