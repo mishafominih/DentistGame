cac036ae22e0d61a343c0806bae5bc27c62f972a
yura159 <47238316+yura159@users.noreply.github.com>
2020-10-13 23:52:04 +0500

    Frontend Menu
    Пока без функционала, просто сделал, как это будет выглядить

---
+++ DentistGame/Assets/Skripts/СonversionScenes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Security.Cryptography.X509Certificates;
+
+public class ConversionScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+            SceneManager.LoadScene("MyScenes", LoadSceneMode.Single);
+    }
+}
