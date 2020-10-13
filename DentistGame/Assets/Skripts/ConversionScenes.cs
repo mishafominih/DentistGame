296105bff5b83b88c9a5921f1e9f004225358b8d
yura159 <47238316+yura159@users.noreply.github.com>
2020-10-14 01:08:40 +0500

    Add button "StartGame"
    Добавил простейший функционал для конпки "StartGame" и "Home". Возился долго, но тебе ли это не знать, Миша. Будто кто-то еще читает эти коммы...

---
+++ DentistGame/Assets/Skripts/ConversionScenes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ConversionScenes : MonoBehaviour
+{
+    public string NameScene;
+
+    public void CreateSecene() => SceneManager.LoadScene(NameScene, LoadSceneMode.Single);
+}
+
