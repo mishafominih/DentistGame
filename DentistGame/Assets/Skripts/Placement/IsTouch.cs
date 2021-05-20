3320fa8a03049602078e6617c2e8e80aa0cc5032
mishafominih <mishagjxn@mail.ru>
2021-05-20 20:31:19 +0500

    добавил новую мини-игру

---
+++ DentistGame/Assets/Skripts/Placement/IsTouch.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsTouch : MonoBehaviour
+{
+    private static int maxLayer = 0;
+    PolygonCollider2D sprite;
+    public Camera cam;
+    private bool isMine = false;
+    SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<PolygonCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount == 1)
+        {
+            Vector3 touch = cam.ScreenToWorldPoint(Input.GetTouch(0).position);
+            if (sprite.OverlapPoint(touch))
+            {
+                isMine = true;
+                int sortingOrder = spriteRenderer.sortingOrder;
+                if (sortingOrder > maxLayer)
+                {
+                    maxLayer = spriteRenderer.sortingOrder;
+                }
+            }
+        }
+        else
+        {
+            if (isMine && GetComponent<SpriteRenderer>().sortingOrder == maxLayer)
+            {
+                if (spriteRenderer.color == Color.green)
+                {
+                    spriteRenderer.color = Color.white;
+                }
+                else
+                {
+                    spriteRenderer.color = Color.green;
+                }
+                maxLayer = 0;
+            }
+            isMine = false;
+        }
+    }
+}
---
+++ DentistGame/Assets/Skripts/Placement/TouthManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TouthManager : MonoBehaviour
+{
+    public int StartChance = 100;
+    private const string SAVE_CHANCE = "PlacementStartChance";
+    private List<Vector3> startPositions;
+    private List<GameObject> tooths;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tooths = GameObject.FindGameObjectsWithTag("Tooth").ToList();
+        startPositions = tooths
+            .Select(x => x.transform.position)
+            .ToList();
+        var chance = PlayerPrefs.GetInt(SAVE_CHANCE);
+        if (chance < StartChance) StartChance = chance;
+        randomTooth(0, tooths.Count / 2);
+        randomTooth(tooths.Count / 2, tooths.Count);
+    }
+
+    private void randomTooth(int start, int end)
+    {
+        for (int i = start; i < end; i++)
+        {
+            for (int j = start; j < end && j != i; j++)
+            {
+                var tooth1 = tooths[i];
+                var tooth2 = tooths[j];
+                if (Random.Range(0, StartChance) < 1)
+                {
+                    swapPosition(tooth1, tooth2);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var greenTouth = tooths
+            .Where(x => x.GetComponent<SpriteRenderer>().color == Color.green)
+            .ToList();
+        if(greenTouth.Count == 2)
+        {
+            swapPosition(greenTouth[0], greenTouth[1]);
+            greenTouth.ForEach(x =>
+            {
+                x.GetComponent<SpriteRenderer>().color = Color.white;
+            });
+        }
+        for(int i = 0; i < tooths.Count; i++)
+        {
+            var toothPos = tooths[i].transform.position;
+            var startPos = startPositions[i];
+            if (Mathf.Abs(Mathf.Abs(startPos.x) - Mathf.Abs(toothPos.x)) > 0.1 ||
+                Mathf.Abs(Mathf.Abs(startPos.y) - Mathf.Abs(toothPos.y)) > 0.1) 
+                return;
+        }
+        PlayerPrefs.SetInt(SAVE_CHANCE, StartChance - 10 > 0 ? StartChance - 10 : 5);
+        SceneManager.LoadScene(7);
+    }
+
+    private static void swapPosition(GameObject first, GameObject second)
+    {
+        var savePos = second.transform.position;
+        second.transform.position = first.transform.position;
+        first.transform.position = savePos;
+    }
+
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.SetInt(SAVE_CHANCE, 100);
+    }
+
+}
